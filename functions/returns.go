11439a654cfa81cce6ed06bac1d6dfd3b2cc7ccd
Fatake <pace_lozano@hotmail.com>
2021-10-12 10:49:02 -0500

    retorno con nombres

--- functions/returns.go
+++ functions/returns.go
@@ -2,25 +2,47 @@ package main
 
 import "fmt"
 
-func suma(valores ...int) int {
+// recibe un slice de valores enteros y devuelve un entero.
+func sum(values ...int) int {
 	total := 0
-	for _, numero := range valores {
-		total += numero
+	for _, num := range values {
+		total += num
 	}
+
 	return total
 }
 
-func getValores(x int) (doble int, triple int, cuatro int) {
-	doble = x * 2
-	triple = x * 3
-	cuatro = x * 4
+// recibe un slice de valores string y los imprime.
+func printNames(names ...string) {
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
+
+// Funcion que retorna multiples valores.
+func getValues(x int) (double int, triple int, quad int) {
+	// return 2 * x, 3 * x, 4 * x // vieja forma.
+	double = 2 * x
+	triple = 3 * x
+	quad = 4 * x
 	return
 }
 
 func main() {
-	fmt.Println(suma(1, 2))
+	// pasando datos en el argumento.
+	fmt.Println(sum(3, 5, 7, 10, 25, 4))
 
-	fmt.Println(suma(1, 2, 3, 4))
+	// Enviando datos en un arreglo.
+	numbers := []int{4, 7, 3}
+	fmt.Println(sum(numbers...))
 
-	fmt.Println(getValores(3))
+	// Llamando a la funcion y pasando strings
+	printNames("Carlos", "Jose", "Maria", "Laura")
+
+	// Imprimiento multiples returns:
+	fmt.Println(getValues(5))
+
+	// capturando multiples returns:
+	d, t, q := getValues(5)
+	fmt.Println(d, t, q)
 }
