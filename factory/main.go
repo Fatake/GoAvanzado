2073c76090e3a42adb0b14dff544c74e9b18b0b0
Fatake <pace_lozano@hotmail.com>
2021-10-12 10:23:24 -0500

    factory end

--- factory/main.go
+++ factory/main.go
@@ -21,7 +21,7 @@ type SMSNotificacion struct {
 // tenemos que hacer un metodo con el nombre exacto de alguna interface
 
 func (SMSNotificacion) SendNotification() {
-	fmt.Print("[+] enviado notificacion via Sms")
+	fmt.Println("[+] enviado notificacion via Sms")
 }
 
 func (SMSNotificacion) GetSender() ISender {
@@ -38,4 +38,55 @@ func (SMSNotificationSender) GetSenderMethod() string {
 
 func (SMSNotificationSender) GetSenderChannel() string {
 	return "WhatssApp"
+}
+
+type EmailNotification struct {
+}
+
+func (EmailNotification) SendNotification() {
+	fmt.Println("[+] Enviando por Email")
+}
+
+func (EmailNotification) GetSender() ISender {
+	return EmailNotificationSender{}
+}
+
+type EmailNotificationSender struct {
+}
+
+func (EmailNotificationSender) GetSenderMethod() string {
+	return "Email"
+}
+
+func (EmailNotificationSender) GetSenderChannel() string {
+	return "Hotmail"
+}
+
+func getNotificationFactory(notificacion string) (INotificationFactory, error) {
+	if notificacion == "SMS" {
+		return &SMSNotificacion{}, nil
+	}
+	if notificacion == "Email" {
+		return &EmailNotification{}, nil
+	}
+	return nil, fmt.Errorf("[!] No se encuentra el tipo de notificacion")
+}
+
+func sendNotification(f INotificationFactory) {
+	f.SendNotification()
+}
+
+func getMethod(f INotificationFactory) {
+	fmt.Println(f.GetSender().GetSenderMethod())
+}
+
+func main() {
+	fmt.Println("[i] Iniciando programa de NotificationFactory")
+	smsFactory, _ := getNotificationFactory("SMS")
+	emailFactory, _ := getNotificationFactory("Email")
+	sendNotification(smsFactory)
+	sendNotification(emailFactory)
+
+	getMethod(smsFactory)
+	getMethod(emailFactory)
 }
