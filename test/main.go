0d7610e7c9fce1b289b8547cbace836e51ce7e0e
Fatake <pace_lozano@hotmail.com>
2021-10-19 10:50:13 -0500

    code covarage

--- test/main.go
+++ test/main.go
@@ -5,6 +5,15 @@ func Suma(x, y int) int {
 }
 
 func GetMax(x, y int) int {
+	/*
+		funcion sin ser testeada llamada
+		code covarage
+		go test -cover # Muestra cuanto porcentaje del codigo no esta testeado
+		go test -coverprofile=salida.out # Genera un archivo
+		go tool cover -func=salida.out # Muestra cuales funciones no estan en testing
+		go tool cover -html=salida.out # Salida en navegador
+
+	*/
 	if x > y {
 		return x
 	}
--- test/main_test.go
+++ test/main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T) {
+func TestSuma(t *testing.T) {
 	tables := []struct {
 		a int
 		b int
@@ -18,10 +18,27 @@ func TestSum(t *testing.T) {
 
 	for _, table := range tables {
 		total := Suma(table.a, table.b)
-		if total != table.r+1 {
+		if total != table.r {
 			t.Errorf("[!] Suma(%d, %d) fue incorrecta, got: %d, want: %d.", table.a, table.b, total, table.r)
 		}
 	}
 }
 
+func TestMax(t *testing.T) {
+	tablas := []struct {
+		a int
+		b int
+		n int
+	}{
+		{4, 2, 4},
+		{3, 1, 3},
+	}
 
+	for _, v := range tablas {
+		max := GetMax(v.a, v.b)
+		if max != v.n {
+			t.Errorf("[!] GetMax fue incorrector, tenia %d, se esperaba %d", max, v.n)
+		}
+	}
+
+}
